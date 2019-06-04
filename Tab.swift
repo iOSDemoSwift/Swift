d3210bc4100e90746f7de9134a7b32cbd4dcb2a9
iospiyushchodvadiya <36265158+iospiyushchodvadiya@users.noreply.github.com>
2019-06-04 23:55:32 +0530

    Delete Tab.swift

--- Tab.swift
+++
@@ -1,7 +0,0 @@
-UITabBar.appearance().barTintColor = API.PrimaryColor()
-self.tabBar.tintColor = UIColor.black
-if #available(iOS 10.0, *) {
-    self.tabBar.unselectedItemTintColor = API.DarkPrimaryColor()
-}
-UITabBar.appearance().backgroundColor = API.PrimaryColor()
-self.tabBarController?.tabBar.backgroundColor = API.PrimaryColor()
